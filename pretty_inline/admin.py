4b766ae7ab9dc28cfe209128cdaecedf9672d946
Jeffrey de Lange <git@jeffreydelange.nl>
2014-02-22 13:35:53 +0100

    Moved template and js to pretty_inline folders

--- pretty_inline/admin.py
+++ pretty_inline/admin.py
@@ -1,18 +1,12 @@
-from django.conf import settings
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.templatetags.admin_static import static
-from django import forms
 
 
 class PrettyInline(InlineModelAdmin):
-    template = 'admin/edit_inline/pretty.html'
+    template = 'pretty_inline/edit_inline/pretty.html'
 
     @property
     def media(self):
-        extra = '' if settings.DEBUG else '.min'
-        js = ['jquery%s.js' % extra, 'jquery.init.js', 'inlines%s.js' % extra, 'pretty_inline.js']
-        if self.prepopulated_fields:
-            js.extend(['urlify.js', 'prepopulate%s.js' % extra])
-        if self.filter_vertical or self.filter_horizontal:
-            js.extend(['SelectBox.js', 'SelectFilter2.js'])
-        return forms.Media(js=[static('admin/js/%s' % url) for url in js])
+        js = super(PrettyInline, self).media
+        js.add_js([static('pretty_inline/js/%s' % 'pretty_inline.js'), ])
+        return js
