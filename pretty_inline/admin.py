58922108416cb2e56b0ddc76fa26a787a6062826
Jeffrey de Lange <git@jeffreydelange.nl>
2014-03-01 11:38:32 +0100

    Initial setup for html template and js

--- pretty_inline/admin.py
+++ pretty_inline/admin.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.templatetags.admin_static import static
 
 
 class PrettyInline(InlineModelAdmin):
     template = 'pretty_inline/edit_inline/pretty.html'
+    extra = 0
 
     @property
     def media(self):
+        files = ['pretty_inline.js']
+
         js = super(PrettyInline, self).media
-        js.add_js([static('pretty_inline/js/%s' % 'pretty_inline.js'), ])
+        js.add_js([static('pretty_inline/js/%s' % f) for f in files])
         return js
