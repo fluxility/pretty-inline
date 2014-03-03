d65491c5b0a2bf1036267b3a0f32e29b465e1eae
Wouter Klein Heerenbrink <wouter@fluxility.com>
2014-03-03 15:32:20 +0100

    Add alternative inline styles

--- pretty_inline/admin.py
+++ pretty_inline/admin.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.templatetags.admin_static import static
 from django.contrib.admin import TabularInline as BaseTabularInline
+from django.contrib.admin import StackedInline as BaseStackedInline
 from django.forms import forms
 
 
@@ -22,6 +23,23 @@ class TabularInline(BaseTabularInline):
 
         return js
 
+
+class StackedInline(BaseStackedInline):
+    template = 'pretty_inline/edit_inline/stacked.html'
+
+    @property
+    def media(self):
+        extra = '' if settings.DEBUG else '.min'
+        js = ['jquery%s.js' % extra, 'jquery.init.js']
+        if self.prepopulated_fields:
+            js.extend(['urlify.js', 'prepopulate%s.js' % extra])
+        if self.filter_vertical or self.filter_horizontal:
+            js.extend(['SelectBox.js', 'SelectFilter2.js'])
+
+        js = forms.Media(js=[static('admin/js/%s' % url) for url in js])
+        js.add_js([static('pretty_inline/js/baseInlineFormset.js')])
+
+        return js
 
 
 class PrettyInline(InlineModelAdmin):
