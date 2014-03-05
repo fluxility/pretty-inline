3fa89598744c98319840207409f7130e735412e6
Wouter Klein Heerenbrink <wouter@fluxility.com>
2014-03-05 17:11:15 +0100

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pretty-inline',
+    version='develop',
+    packages=['pretty_inline',],
+    package_dir={'': 'example_project'},
+    url='',
+    license='',
+    author='Fluxility',
+    author_email='contact@fluxility.com',
+    description='Improved layout for inline admin interface in Django'
+)
